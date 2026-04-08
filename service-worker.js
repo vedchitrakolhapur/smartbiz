14e4bc3429911b184d5bccc21ade4c5fb039b8d1
vedchitrakolhapur <vedchitrakolhapur@gmail.com>
2026-04-08 12:22:05 +0530

    Auto Update: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-      const CACHE_NAME = 'biz-app-v2';
+      const CACHE_NAME = 'biz-app-v3';
       self.addEventListener('install', (e) => { self.skipWaiting(); });
       self.addEventListener('activate', (e) => { e.waitUntil(clients.claim()); });
       self.addEventListener('fetch', (e) => {
